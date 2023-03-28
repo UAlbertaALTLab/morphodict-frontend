91085c31f903b77aef280a090634025bc41ac50e
Jolene Poulin <jcpoulin@ualberta.ca>
2023-03-28 15:18:55 -0600

    remove: extra docker files

--- docker/inner_deploy.py
+++
@@ -1,6 +0,0 @@
-def run(call):
-    """Perform post-git-pull deploy operations"""
-
-    call(["./helper.py", "setup"])
-    call(["docker-compose", "pull"])
-    call(["docker-compose", "up", "-d", "--remove-orphans"])
