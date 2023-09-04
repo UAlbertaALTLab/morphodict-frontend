95cfde149d36f6b3f3c4ea8d527b4c8190e82aa1
M1Al3x <90932420+M1Al3x@users.noreply.github.com>
2023-09-04 17:18:01 -0600

    Update of recordings to load in the frontend

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -8,14 +8,38 @@ index  : the resulting index of the item from the returned array.
 Goal   : The purpose of this page is to display the search results gotten from user search. This is a single display of the word before a user picks the current file. 
        
 */
-
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Tooltip, OverlayTrigger, Button} from "react-bootstrap";
 import {Link, Redirect} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faVolumeUp, faInfoCircle, faBorderNone} from '@fortawesome/free-solid-svg-icons'
 import LikeWord from "./LikeWord";
-
+const LANGUAGE_CODES = getLanguageCodesFromLocation();
+const BASE_URL = "https://speech-db.altlab.app";
+function getLanguageCodesFromLocation() {
+    const location = window.location.toString();
+    console.log("Hello");
+    const audio_source = getCookie("audio_source");
+    if (audio_source && audio_source != "both") {
+      return [audio_source];
+    } else if (location.includes(`itwewina`) || location.includes(`crk`)) {
+      if (getCookie("synthesized_audio") == "yes") {
+        return [`maskwacis`, `moswacihk`, `synth`];
+      } else return [`maskwacis`, `moswacihk`];
+    } else if (location.includes(`itwiwina`) || location.includes(`cwd`))
+      return [`woodscree`];
+    else if (location.includes(`gunaha`) || location.includes(`srs`))
+      return [`tsuutina`];
+    else if (location.includes(`nihiitono`)) return [`arapaho`];
+    else if (location.includes(`guusaaw`) || location.includes(`hdn`))
+      return [`haida`];
+    return [`maskwacis`];
+}
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
 function getInflectionalCategory(wordInformation) {
     try {
         return wordInformation["lemma_wordform"]["inflectional_category_linguistic"] +
@@ -29,6 +53,7 @@ function getInflectionalCategory(wordInformation) {
             ")";
     }
 }
+
 
 function getInflectionalCategoryPlainEnglish(wordInformation) {
     try {
@@ -46,9 +71,133 @@ function getInflectionalCategoryPlainEnglish(wordInformation) {
 
 const SearchSection = (props) => {
 
+    let [soundBtn, setBtn] = useState();
     let [persistTooltip, setPersistTooltip] = useState(false);
     let [showTooltip, setShowTooltip] = useState(false);
+    let sound = '';
+    useEffect(() => {
+        async function fetchFirstRecordingURL(wordform) {
+            let response = await getRecordingsForWordformsFromMultipleUrls([wordform]);
+            return mapWordformsToBestRecordingURL(response).get(wordform);
+        }
+        async function getRecordingsForWordformsFromMultipleUrls(requestedWordforms) {
+            let retObject = { matched_recordings: [], not_found: [] };
+            for (let LANGUAGE_CODE of LANGUAGE_CODES) {
+              let bulkApiUrl = `${BASE_URL}/${LANGUAGE_CODE}/api/bulk_search`;
+              let response = await fetchRecordingUsingBulkSearch(
+                bulkApiUrl,
+                requestedWordforms
+              );
+              retObject["matched_recordings"] = retObject["matched_recordings"].concat(
+                response["matched_recordings"]
+              );
+              retObject["not_found"] = retObject["not_found"].concat(
+                response["not_found"]
+              );
+            }
+            return retObject;
+        }
+        function mapWordformsToBestRecordingURL(response) {
+            let wordform2recordingURL = new Map();
+          
+            for (let result of response["matched_recordings"]) {
+              let wordform = result["wordform"];
+          
+              if (!wordform2recordingURL.has(wordform)) {
+                // Assume the first result returned is the best recording:
+                wordform2recordingURL.set(wordform, result["recording_url"]);
+              }
+            }
+          
+            return wordform2recordingURL;
+        }
+        async function fetchRecordingUsingBulkSearch(bulkApiUrl, requestedWordforms) {
+            let batches = chunk(requestedWordforms);
+          
+            let allMatchedRecordings = [];
+            let allNotFound = [];
+          
+            for (let batch of batches) {
+              let response = await _fetchRecordingUsingBulkSearch(bulkApiUrl, batch);
+          
+              response["matched_recordings"].forEach((rec) =>
+                allMatchedRecordings.push(rec)
+              );
+              response["not_found"].forEach((rec) => allNotFound.push(rec));
+            }
+          
+            return {
+              matched_recordings: allMatchedRecordings,
+              not_found: allNotFound,
+            };
+        }
+        async function _fetchRecordingUsingBulkSearch(bulkApiUrl, requestedWordforms) {
+            // Construct the query parameters: ?q=word&q=word2&q=word3&q=...
+            let searchParams = new URLSearchParams();
+            for (let wordform of requestedWordforms) {
+              searchParams.append("q", wordform);
+            }
+            let url = new URL(bulkApiUrl);
+            url.search = searchParams;
+          
+            let response = await fetch(url);
+            if (!response.ok) {
+              throw new Error("Could not fetch recordings");
+            }
+          
+            return response.json();
+        }
+        function chunk(collection) {
+            const MAX_BATCH_SIZE = 30;
+          
+            // Chunk items iteratively, sort of like packing moving boxes, adding items
+            // to one box at at time until the box gets full, and then moving on to a
+            // new, empty box:
+            let chunks = [[]];
+            for (let item of collection) {
+              // invariant: the array of all chunks has at least one chunk
+              let currentChunk = chunks[chunks.length - 1];
+          
+              if (currentChunk.length >= MAX_BATCH_SIZE) {
+                // The current chunk is full!
+                // We can't add anymore items so start a new chunk.
+                currentChunk = [];
+                chunks.push(currentChunk);
+              }
+          
+              // invariant: currentChunk.length < batch size:
+              // ∴ it's safe to add an item to the current chunk
+              currentChunk.push(item);
+            }
+          
+            return chunks;
+        }
 
+        let wordform = displayWord();
+        sound = fetchFirstRecordingURL(wordform);
+        //Information on api only learned on 2/24/2022 moved into sp3
+        if (sound !== "") {
+            let soundBtnRes = (
+                <Button variant="btn bg-white rounded" onMouseDown={(e)=> {e.preventDefault()}}
+                        id="soundbutton"
+                        onClick={handleSoundPlay}
+                        data-cy="playRecording"
+                >
+                    <FontAwesomeIcon icon={faVolumeUp}
+                    id="soundicon"
+                    size="xl"
+                    onMouseOver={handleSoundIconOnMouseOver}
+                    onMouseLeave={handleSoundIconMouseLeave}
+                    style={{color:"#286995", marginLeft:"-12px"}}
+                    />
+                </Button>
+            );
+            console.log(soundBtnRes);
+            setBtn(soundBtnRes);
+        }
+        console.log(soundBtn);
+        console.log(sound);
+      }, []);
     const getInformation = () => {
         if (wordInformation["relabelled_fst_analysis"]) {
             switch (settings.label) {
@@ -145,12 +294,9 @@ const SearchSection = (props) => {
     }
 
     let infoBtn = "";
-    let soundBtn = "";
-    let sound = wordInformation["recording"];
     let wordBtn = "";
     let lemmaWordform = getLemmaWordform();
-
-    const handleSoundPlay = () => {
+    async function handleSoundPlay() {
         const audio = new Audio(sound);
         audio.play();
     };
@@ -185,24 +331,6 @@ const SearchSection = (props) => {
         );
     }
 
-    //Information on api only learned on 2/24/2022 moved into sp3
-    if (sound !== "") {
-        soundBtn = (
-            <Button variant="btn bg-white rounded" onMouseDown={(e)=> {e.preventDefault()}}
-                    id="soundbutton"
-                    onClick={handleSoundPlay}
-                    data-cy="playRecording"
-                    >
-                <FontAwesomeIcon icon={faVolumeUp}
-                id="soundicon"
-                size="xl"
-                onMouseOver={handleSoundIconOnMouseOver}
-                onMouseLeave={handleSoundIconMouseLeave}
-                style={{color:"#286995", marginLeft:"-12px"}}
-                />
-            </Button>
-        );
-    }
 
     const getEmoticon = () => {
         try {
@@ -285,10 +413,10 @@ const SearchSection = (props) => {
                     </OverlayTrigger> : null}
                 </div> 
 
-                {infoSoundButtons ? <div style= {{marginTop: "0.1em"}} className="definition-title__play-icon">{soundBtn}</div> : null}
+                {soundBtn =><div style= {{marginTop: "0.1em"}} className="definition-title__play-icon">{soundBtn}</div>}
                 </div>
             </div>
-
+            <script src="pathToJs" defer></script>
             {shouldNotDisplayFormOf() ? <LikeWord
                 wordform={wordInformation}
 
@@ -312,6 +440,7 @@ const SearchSection = (props) => {
                     </li>
                 ))}
             </ul>
+
             {shouldNotDisplayFormOf() ? <></> :
                 <><p><i>Form of:</i></p>
                     <SearchSection
